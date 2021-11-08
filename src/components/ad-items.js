88500d7be145b914e7ce9aa9b3d109c03aa47319
tKamioka <kamitamo1002@gmail.com>
2021-11-08 10:16:26 +0900

    Add 11/6-7 result.

--- src/components/ad-items.js
+++ src/components/ad-items.js
@@ -2,16 +2,22 @@
 export const adItems = [
 
     {
-        Url: "https://www.amazon.co.jp/gp/product/B00115SZKU/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B00115SZKU&linkCode=as2&tag=esb0c-22&linkId=d9611b603ac8c463e6007373f1d67fc9",
-        ImgUrl: "//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=B00115SZKU&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=esb0c-22",
-        Title: "怪我防止にテーピング" ,
-        Text: "3M(スリーエム) テーピング50mm 6巻",
-    },
-    {
         Url: "https://www.amazon.co.jp/gp/product/B00FB9GGZS/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B00FB9GGZS&linkCode=as2&tag=esb0c-22&linkId=fbb407d19515521b815f04f5c0a7bd0d",
         ImgUrl: "//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=B00FB9GGZS&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=esb0c-22",
         Title: "成長期10代の決定版",
         Text: "成長の鍵は栄養バランス！子供の身長サプリメント カラダアルファ(α)",
+    },
+    {
+        Url: "https://www.amazon.co.jp/gp/product/B08VR99HJR/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B08VR99HJR&linkCode=as2&tag=esb0c-22&linkId=2cd0b2207f4ac485a1813bb22c8464ed",
+        ImgUrl: "//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=B08VR99HJR&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=esb0c-22",
+        Title: "2リットル水筒" ,
+        Text: "オフシーズン今がお買い得 サーモス",
+    },
+    {
+        Url: "https://www.amazon.co.jp/gp/product/B00115SZKU/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B00115SZKU&linkCode=as2&tag=esb0c-22&linkId=d9611b603ac8c463e6007373f1d67fc9",
+        ImgUrl: "//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=B00115SZKU&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=esb0c-22",
+        Title: "怪我防止にテーピング" ,
+        Text: "3M(スリーエム) テーピング50mm 6巻",
     },
 
     // {
@@ -21,12 +27,6 @@ export const adItems = [
     //     Text: "フットサポーター Activital(アクティバイタル)",
     // },
 
-    {
-        Url: "https://www.amazon.co.jp/gp/product/B08VR99HJR/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B08VR99HJR&linkCode=as2&tag=esb0c-22&linkId=2cd0b2207f4ac485a1813bb22c8464ed",
-        ImgUrl: "//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=B08VR99HJR&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=esb0c-22",
-        Title: "2リットル水筒" ,
-        Text: "オフシーズン今がお買い得 サーモス",
-    },
 ]
 
 
