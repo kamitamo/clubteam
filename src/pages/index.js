3c1e74620ac814f6117ced2066734c7643d1656d
tKamioka <kamitamo1002@gmail.com>
2022-12-19 09:03:22 +0900

    Add 1217-1218 contents.

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,8 @@ const HomePage = ({ data }) => {
                         </header>
                         <ul className="actions">
                             <li><a href="https://instagram.com/fc_esblanco?r=nametag" title="Instagram" target="_blank" rel="noopener noreferrer">Instagram(@fc_esblanco)はじめました</a></li><br/>
+                            <li><Link to="/journal"><u>12/17,12/18の試合結果</u>を更新しました</Link></li><br/>
                             <li><Link to="/journal"><u>12/10,12/11の試合結果</u>を更新しました</Link></li><br/>
-                            <li><Link to="/journal"><u>12/03,12/04の試合結果</u>を更新しました</Link></li><br/>
                         </ul>
                     </div>
                 </section>
